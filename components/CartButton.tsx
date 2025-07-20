064e4ae5a79e788ff5693be0530a61a37e1ce0d6
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-07-20 16:31:39 +0530

    assets, constants and nativewind setup

---
+++ components/CartButton.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CartButton() {
+  return (
+    <View>
+      <Text>CartButton</Text>
+    </View>
+  )
+}
